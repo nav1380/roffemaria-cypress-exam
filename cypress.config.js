f807d91944a57a0cbc609997e3e83833c5f975ad
Godwy <godwynnejoshuadimayuga@gmail.com>
2025-05-04 18:42:58 +0800

    ahahaha

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,21 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+const dotenv = require("dotenv");
+require("dotenv").config();
 
 module.exports = defineConfig({
   projectId: "yteuhi",
+  experimentalStudio: true,
+  retries: 3,
+  // testIsolation: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
