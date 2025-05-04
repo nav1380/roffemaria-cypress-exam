6bf8c676b30654fecaf94f9187c72209e270ea88
Godwy <godwynnejoshuadimayuga@gmail.com>
2025-05-04 14:25:52 +0800

    ahahaha

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "yteuhi",
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    reportDir: "cypress/reports/html",
-    overwrite: false,
-    html: true,
-    json: true
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-      // you can add more plugins here later if needed
+      // implement node event listeners here
     },
-    specPattern: "cypress/e2e/**/*.cy.js", // optional if you want to be explicit
-    supportFile: false // optional: use true if you have a support file
-  }
+  },
 });
